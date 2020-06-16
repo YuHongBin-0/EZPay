1e9e4dee20456e5615f829ec0a526d08098ff2b8
Ng Chin Yan <ngchinyan101@gmail.com>
2020-06-16 15:45:33 +0800

    added carousel

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonSlides } from '@ionic/angular'
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  slideOpts = { loop: true };
+
   constructor() {}
+
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+  }
 
 }
