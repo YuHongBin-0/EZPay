cccc5496d0086a2a7a8835fa52a7ccb7f2bd20f8
Ng Chin Yan <ngchinyan101@gmail.com>
2020-06-17 10:40:45 +0800

    solve the card error

---
+++ src/app/components/expandable/expandable.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expandable',
+  templateUrl: './expandable.component.html',
+  styleUrls: ['./expandable.component.scss'],
+})
+export class ExpandableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
