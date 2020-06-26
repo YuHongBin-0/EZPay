68a954938f267e5ec22a1d3e341504756c2f8beb
Ng Chin Yan <ngchinyan101@gmail.com>
2020-06-26 20:58:08 +0800

    added qr scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { AppComponent } from './app.component';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,6 +5,10 @@ import { PayPage } from '../pay/pay.page';
 import { ScanPage } from '../scan/scan.page';
 import { RequestPage } from '../request/request.page';
 import { MyqrPage } from '../myqr/myqr.page';
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from "@ionic-native/barcode-scanner/ngx";
 
 @Component({
   selector: 'app-tab2',
@@ -12,12 +16,22 @@ import { MyqrPage } from '../myqr/myqr.page';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  
+  encodeData: any;
+  scannedData: {};
+  barcodeScannerOptions: BarcodeScannerOptions;
 
   slideOpts = { loop: true };
 
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private barcodeScanner: BarcodeScanner) {
+    this.encodeData = "https://www.FreakyJolly.com";
+    //Options
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+    };
+  }
+
+
   async openPay() {
     const modal = await this.modalController.create({
       component: PayPage
@@ -25,11 +39,30 @@ export class Tab2Page {
     return await modal.present();
   }
 
-  async openScan() {
-    const modal = await this.modalController.create({
-      component: ScanPage
-    });
-    return await modal.present();
+  openScan() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        alert("Barcode data " + JSON.stringify(barcodeData));
+        this.scannedData = barcodeData;
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+  encodedText() {
+    this.barcodeScanner
+      .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData)
+      .then(
+        encodedData => {
+          console.log(encodedData);
+          this.encodeData = encodedData;
+        },
+        err => {
+          console.log("Error occured : " + err);
+        }
+      );
   }
 
   async openRequest() {
