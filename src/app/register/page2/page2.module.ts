101c7e373dedeba5bc8e5f78e0fd9c31bb5e4e99
Ng Chin Yan <ngchinyan101@gmail.com>
2020-07-22 13:13:56 +0800

    finished register design

--- src/app/register/page2/page2.module.ts
+++ src/app/register/page2/page2.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { Page2PageRoutingModule } from './page2-routing.module';
 
@@ -11,7 +12,7 @@ import { Page2Page } from './page2.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     Page2PageRoutingModule
   ],
--- src/app/register/page2/page2.page.ts
+++ src/app/register/page2/page2.page.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.page.html',
   styleUrls: ['./page2.page.scss'],
 })
+
 export class Page2Page implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+  get name() {
+    return this.registrationForm.get("profile.name");
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
+  get department() {
+    return this.registrationForm.get('profile.department');
+  }
+  get NRIC() {
+    return this.registrationForm.get('profile.NRIC');
+  }
+  get balance() {
+    return this.registrationForm.get('profile.balance');
+  }
+  public errorMessages = {
+    name: [
+      { type: 'required', message: 'Name is required' },
+      { type: 'maxlength', message: 'Name cant be longer than 100 characters' }
+    ],
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Please enter a valid email address' }
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' },
+      
+    ],
+    department: [
+      { type: 'required', message: 'Department name is required' },
+      {
+        type: 'maxlength',
+        message: 'Department name cant be longer than 100 characters'
+      }
+    ],
+    NRIC: [
+      { type: 'required', message: 'NRIC is required' },
+      {
+        type: 'maxlength',
+        message: 'NRIC cant be longer than 9 characters'
+      }
 
-  constructor() { }
-
+    ],
+    balance: [
+      { type: 'required', message: 'Balance Amount is required' },
+      {
+        type: 'pattern',
+        message: 'Please enter a valid Balance Amount'
+      }
+    ]
+  };
+  registrationForm = this.formBuilder.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$')
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        
+      ]
+    ],
+    profile: this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      department: ['', [Validators.required, Validators.maxLength(100)]],
+      NRIC: ['', [Validators.required, Validators.maxLength(9)]],
+      balance: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]{0,4}[.][0-9]{0,2}$')]
+      ]
+    })
+  });
+  public submit() {
+    console.log(this.registrationForm.value);
+  }
   ngOnInit() {
   }
-
 }
+  
+
