e4e022c02ad91f3ce4ee2241c3d4911ada3e258d
cjmmm <1801190h@student.tp.edu.sg>
2020-07-27 20:51:06 +0800

    report pg

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -18,3 +18,35 @@ import { ReportPage } from './report.page';
   declarations: [ReportPage]
 })
 export class ReportPageModule {}
+
+
+
+
+
+import { Directive, ElementRef, HostListener } from '@angular/core';
+ 
+@Directive({
+  selector: '[autosize]' // Attribute selector
+})
+export class AutosizeDirective {
+ 
+  @HostListener('input', ['$event.target'])
+  onInput(textArea:HTMLTextAreaElement):void {
+    this.adjust();
+  }
+ 
+  constructor(public element:ElementRef) {
+    console.log('Hello AutosizeDirective Directive');
+  }
+ 
+  ngOnInit():void {
+    setTimeout(() => this.adjust(), 0);
+  }
+ 
+  adjust():void {
+    const textArea = this.element.nativeElement.getElementsByTagName('textarea')[0];
+    textArea.style.overflow = 'hidden';
+    textArea.style.height = 'auto';
+    textArea.style.height = textArea.scrollHeight + 'px';
+  }
+}
