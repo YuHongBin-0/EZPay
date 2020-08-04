5d1a15db6172b1b5cd294b9f17f0d5d453aeeecb
NowFile <1804380B@student.tp.edu.sg>
2020-08-04 20:23:49 +0800

    testing

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit{
 
-  constructor(private router: Router) {}
+  users: Observable<any>
+  userId: string;
+  isStudent:boolean | null | undefined = true;
 
+  constructor(private router: Router, private afAuth: AngularFireAuth, private afDatabase: AngularFireDatabase) {}
+
+  ngOnInit(){
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var role = (snapshot.val() && snapshot.val().role).toString();
+      // var role = "vendor";
+      console.log('userRole: ' + role)
+      if (role.toString() === "student") {
+        this.isStudent = true;
+        console.log(this.isStudent);
+      }
+      else if (role === "vendor") {
+        this.isStudent = false;
+        console.log(this.isStudent);
+      }
+      return this.isStudent;
+    })
+
+  }
   gotoHome (){
-    this.router.navigateByUrl('/tabs/tab2')
+    this.router.navigateByUrl('/tabs/tab2');
+
   }
 }
