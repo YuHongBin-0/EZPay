1e76d2bb49420ccc5cfb05f9521f2b57a8ba02ff
Ng Chin Yan <email@example.com1801187D@student.tp.edu.sg>
2020-08-06 19:19:52 +0800

    pushed

--- src/app/viewreport/viewreport.page.ts
+++ src/app/viewreport/viewreport.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-viewreport',
@@ -10,12 +11,21 @@ import {MatExpansionModule} from '@angular/material/expansion';
 export class ViewreportPage implements OnInit {
 
   infos = [];
+  id = this.afDatabase.object(`reports/`)
   ref = firebase.database().ref('reports/');
-  constructor(public matExpansionModule: MatExpansionModule) { }
+  constructor(public matExpansionModule: MatExpansionModule, private afDatabase: AngularFireDatabase,) { }
 
-  ngOnInit() { this.ref.on('value', resp => {
+  ngOnInit() { 
+    this.ref.on('value', resp => {
     this.infos = snapshotToArray(resp);
   });
+
+  }
+
+  async deletePost(infos: string) {
+    console.log(this.infos);
+     var id = this.afDatabase.object(`reports/`)
+    await this.afDatabase.object(`reports/${infos}/`).remove();
 
   }
 
