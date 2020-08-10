fb9aee9bda9be0ca205e2ede8b7594257ab136ae
Ng Chin Yan <email@example.com1801187D@student.tp.edu.sg>
2020-08-11 05:30:22 +0800

    added for shop item

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { AngularFireDatabase } from 'angularfire2/database'
-import { ShopItemService } from '../services/shop-item.service'
 
 @Component({
   selector: 'app-tab3',
@@ -11,35 +10,36 @@ import { ShopItemService } from '../services/shop-item.service'
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  infos = [];
-  // infos1 = [];
-  ref = firebase.database().ref('stallItem/');
-
- 
   
-  // ref1 = firebase.database().ref('stallItem/goods');
-  constructor(public matExpansionModule: MatExpansionModule ,private fetch: ShopItemService ) { }
 
+  shopDeets = [];
+  shopItems = [];
+  refShop = firebase.database().ref('stalls');
+  refItems = firebase.database().ref('products');
+
+
+
+  constructor(public matExpansionModule: MatExpansionModule  ) { }
   ngOnInit() { 
-    this.ref.on('value', resp => {
-    this.infos = snapshotToArray(resp);
-  });
 
-  console.log(this.ref)
-
- 
-
-  // this.ref1.on('value', resp => {
-  //   this.infos1 = snapshotToArray1(resp);
-  // });
-
+    var stall;
+    this.refShop.on('value', resp => {
+      this.shopDeets = snapshotToArray(resp);
+      this.shopDeets.forEach(element => {
+        stall = element.key;
+        this.refItems .orderByChild("stall")
+        .equalTo(stall)
+        .on('value', resp1 => {
+          this.shopItems = snapshotToArray(resp1);
+          console.log(this.shopItems);  
+        });
+      });
+    });
   }
 
 }
 
-
-export const snapshotToArray = snapshot => {  
+export const snapshotToArray = snapshot => {
   const returnArr = [];
 
   snapshot.forEach(childSnapshot => {
@@ -51,14 +51,5 @@ export const snapshotToArray = snapshot => {
   return returnArr.reverse();
 };
 
-// export const snapshotToArray1 = snapshot => {  
-//   const returnArr = [];
 
-//   snapshot.forEach(childSnapshot => {
-//     const item = childSnapshot.val();
-//     item.key = childSnapshot.key;
-//     returnArr.push(item);
-//   });
 
-//   return returnArr.reverse();
-// };
