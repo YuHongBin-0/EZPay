4a06a57d4660e44b599dd0e7a88f45b7a6046bde
khooyechen123 <1801675H@student.tp.edu.sg>
2020-08-18 20:18:17 +0800

    cp

--- src/app/viewreport/viewreport.page.ts
+++ src/app/viewreport/viewreport.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-viewreport',
@@ -13,23 +14,49 @@ export class ViewreportPage implements OnInit {
   infos = [];
   ref = firebase.database().ref('reports/');
 
-  constructor(public matExpansionModule: MatExpansionModule, private afDatabase: AngularFireDatabase,) {
+  // tslint:disable-next-line: max-line-length
+  constructor(public matExpansionModule: MatExpansionModule, private afDatabase: AngularFireDatabase,
+              public alertController: AlertController) {
 
    }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.ref.on('value', resp => {
     this.infos = snapshotToArray(resp);
   });
   }
-  
+
+  async presentAlertConfirm(key) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmation',
+      message: 'Changed to Solve',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Okay',
+          handler: async () => {
+            await this.afDatabase.object(`reports/${key}/`).remove();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
   async deletePost(key) {
       await this.afDatabase.object(`reports/${key}/`).remove();
     }
 
 }
 
-export const snapshotToArray = snapshot => {  
+export const snapshotToArray = snapshot => {
   const returnArr = [];
 
   snapshot.forEach(childSnapshot => {
