eae903a12e89a8048fe6799545128bdc71d1713a
cjmmm <1801190h@student.tp.edu.sg>
2020-08-18 14:28:12 +0800

    transac history

--- src/app/adm-history/adm-history.page.ts
+++ src/app/adm-history/adm-history.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adm-history',
@@ -8,9 +13,30 @@ import {MatExpansionModule} from '@angular/material/expansion';
 })
 export class AdmHistoryPage implements OnInit {
 
-  constructor(public matExpansionModule: MatExpansionModule) { }
+  reference = [];
+  refItems = firebase.database().ref('transaction');
+
+  constructor(public matExpansionModule: MatExpansionModule, public afAuth: AngularFireAuth,
+    public afdatabase: AngularFireDatabase, private router: Router) { }
+
+  panelOpenState = false;
 
   ngOnInit() {
-  }
+      this.refItems.on('value', resp => {
+        this.reference = snapshotToArray(resp);
+      });
+    }
 
 }
+
+export const snapshotToArray = snapshot => {
+  const returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr.reverse();
+};
