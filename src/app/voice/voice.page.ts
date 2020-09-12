185abb0bd0f867d8bf772b1d244e0783c5cd72c1
Ng Chin Yan <email@example.com1801187D@student.tp.edu.sg>
2020-09-12 21:17:14 +0800

    improve on voice

--- src/app/voice/voice.page.ts
+++ src/app/voice/voice.page.ts
@@ -3,9 +3,6 @@ import { Base64 } from '@ionic-native/base64/ngx'
 import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
 import {MediaCapture,MediaFile,CaptureError} from '@ionic-native/media-capture/ngx';
 import { File, FileEntry } from '@ionic-native/file/ngx';
-import { Media, MediaObject } from '@ionic-native/media/ngx';
-import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
-import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
 import { HttpClient } from '@angular/common/http';
 import { HTTP } from '@ionic-native/http/ngx';
 import { LoadingController } from '@ionic/angular';
@@ -31,10 +28,6 @@ export class VoicePage implements OnInit {
     private base64: Base64,
     private mediaCapture: MediaCapture,
     private file: File,
-    private media: Media,
-    private streamingMedia: StreamingMedia,
-    private photoViewer: PhotoViewer,
-    private actionSheetController: ActionSheetController,
     private plt: Platform,
     public http: HttpClient,
     private nativeHttp: HTTP,
@@ -61,10 +54,8 @@ export class VoicePage implements OnInit {
     this.base64.encodeFile(filePath).then((base64Audio: string) => {
       this.base64text = base64Audio.replace("data:image/*;charset=utf-8;base64,","")
 
-      window.alert(this.base64text);
-
     }, (err) => {
-      window.alert("readAsDataURL: " + err);
+      
     })
 
   }
@@ -121,118 +112,8 @@ export class VoicePage implements OnInit {
     );
   }
 
-  async selectMedia() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'What would you like to add?',
-      buttons: [
-        {
-          text: 'Capture Image',
-          handler: () => {
-            this.captureImage();
-          }
-        },
-        {
-          text: 'Record Video',
-          handler: () => {
-            this.recordVideo();
-          }
-        },
-        {
-          text: 'Record Audio',
-          handler: () => {
-            this.recordAudio();
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-    await actionSheet.present();
-  }
- 
-  
- 
-  captureImage() {
-    this.mediaCapture.captureImage().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
-  }
- 
   recordAudio() {
-    this.mediaCapture.captureAudio().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
+    this.mediaCapture.captureAudio();
   }
  
-  recordVideo() {
-    this.mediaCapture.captureVideo().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
-  }
-
-  copyFileToLocalDir(fullPath) {
-    let myPath = fullPath;
-    // Make sure we copy from the right location
-    if (fullPath.indexOf('file://') < 0) {
-      myPath = 'file://' + fullPath;
-    }
- 
-    const ext = myPath.split('.').pop();
-    const d = Date.now();
-    const newName = `${d}.${ext}`;
- 
-    const name = myPath.substr(myPath.lastIndexOf('/') + 1);
-    const copyFrom = myPath.substr(0, myPath.lastIndexOf('/') + 1);
-    const copyTo = this.file.dataDirectory + MEDIA_FOLDER_NAME;
- 
-    this.file.copyFile(copyFrom, name, copyTo, newName).then(
-      success => {
-        this.loadFiles();
-      },
-      error => {
-        console.log('error: ', error);
-      }
-    );
-  }
- 
-  openFile(f: FileEntry) {
-    if (f.name.indexOf('.wav') > -1) {
-      // We need to remove file:/// from the path for the audio plugin to work
-      const path =  f.nativeURL.replace(/^file:\/\//, '');
-      const audioFile: MediaObject = this.media.create(path);
-      audioFile.play();
-    } else if (f.name.indexOf('.MOV') > -1 || f.name.indexOf('.mp4') > -1) {
-      // E.g: Use the Streaming Media plugin to play a video
-      this.streamingMedia.playVideo(f.nativeURL);
-    } else if (f.name.indexOf('.jpg') > -1) {
-      // E.g: Use the Photoviewer to present an Image
-      this.photoViewer.show(f.nativeURL, 'MY awesome image');
-    }
-  }
- 
-  deleteFile(f: FileEntry) {
-    const path = f.nativeURL.substr(0, f.nativeURL.lastIndexOf('/') + 1);
-    this.file.removeFile(path, f.name).then(() => {
-      this.loadFiles();
-    }, err => console.log('error remove: ', err));
-  }
-
-
 }
